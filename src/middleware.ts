854a5914bacdce44bca2fb7f3f2c603d8d33198a
Namanyay Goel <mail@namanyayg.com>
2025-05-01 14:32:54 -0400

    fix cf build....

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,9 @@
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import { getRequestContext } from '@cloudflare/next-on-pages';
 
 export const config = {
   matcher: '/:slug*'
 };
 
-export async function middleware(request: NextRequest) {
-  // You can move any edge-specific logic here
+export async function middleware() {
   return NextResponse.next();
 } 
